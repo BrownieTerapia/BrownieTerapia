308f29f8ad5be3dc6230c7b472a0675177f27c26
BrownieTerapia <brownie.terapia.pe.2021@gmail.com>
2024-09-18 22:11:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,24 @@ L.tileLayer('https://.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Añade un marcador al mapa
-var marker = L.marker([-18.017313, -70.237872]).addTo(map);
+var marker1 = L.marker([-18.017313, -70.237872]).addTo(map);
+marker1.bindPopup("<b>Joabi</b><br>Parque Jesus María.").openPopup();
 
-// Añade un popup al marcador
-marker.bindPopup("<b>Joabi</b><br>Parque Jesus María.").openPopup();
+var marker2 = L.marker([-18.0095, -70.2517]).addTo(map);
+marker2.bindPopup("<b>Golosilandia</b><br>Calderon de la Barca.");
+
+var marker3 = L.marker([-18.037459, -70.25109]).addTo(map);
+marker3.bindPopup("<b>Tienda Etérea</b><br>Perez gamboa.");
+
+var marker4 = L.marker([-18.0167047, -70.25180]).addTo(map);
+marker4.bindPopup("<b>Tloc Cookies</b><br>Descripción de la tienda 4.");
+
+var marker5 = L.marker([-18.0129808, -70.24936]).addTo(map);
+marker5.bindPopup("<b>Comerical Jhilmar</b><br>Descripción de la tienda 5.");
+
+var marker6 = L.marker([-18.0073255, -70.2475495]).addTo(map);
+marker6.bindPopup("<b>Yogurt de casa Sabi</b><br>Descripción de la tienda 6.");
+
+var marker7 = L.marker([-18.00568, -70.22544]).addTo(map);
+marker7.bindPopup("<b>Cafetería Facsa Upt</b><br>Descripción de la tienda 7.");
+
